cdbedb5b890ff7295963e7ef075ec0af788c3f7a
polina904 <polina_korolyova02@mail.ru>
2022-02-12 12:00:03 +0300

    2 задание

---
+++ 2Ex.js
@@ -0,0 +1,18 @@
+let age;
+console.log (typeof age); 
+let num = 50;
+console.log (typeof num);
+let cif = 10203045n;
+console.log (typeof cif); 
+let str = 'Мама';
+console.log (typeof str); 
+let TF = true;
+console.log (typeof TF); 
+let sim = Symbol();
+console.log (typeof sim); 
+let user = {age:19};
+console.log (typeof user); 
+let per = null;
+console.log (typeof per); 
+let us = function(){};
+console.log (typeof us); 
